a7545a372edc0e19f2b83374a9db0faac41c800d
Chandler De Angelis <chandler.dea@me.com>
2019-07-19 17:46:56 -0700

    Added HTTPModelController

---
+++ Sources/HTTPKit/HTTPModelController.swift
@@ -0,0 +1,52 @@
+//
+//  HTTPModelController.swift
+//  HTTPKit
+//
+//  Created by Chandler De Angelis on 7/19/19.
+//  Copyright © 2019 Chandlerdea LLC. All rights reserved.
+//
+
+import Foundation
+
+public protocol HTTPModelController: HTTPNetworkController {
+
+    func getModels<Model: Codable & Equatable>(with requestBuilder: HTTP.RequestBuilder, in session: URLSession, completion: @escaping (Result<[Model], Error>) -> Void)
+    
+    func getModel<Model: Codable & Equatable>(with requestBuilder: HTTP.RequestBuilder, in session: URLSession, completion: @escaping (Result<Model, Error>) -> Void)
+    
+    func postModel<Model: Codable & Equatable>(_ model: Model, with requestBuilder: HTTP.RequestBuilder, in session: URLSession, completion: @escaping (Result<Model, Error>) -> Void)
+    
+    func putModel<Model: Codable & Equatable>(_ model: Model, with requestBuilder: HTTP.RequestBuilder, in session: URLSession, completion: @escaping (Result<Model, Error>) -> Void)
+    
+    func deleteModel<Model: Codable & Equatable>(_ model: Model, with requestBuilder: HTTP.RequestBuilder, in session: URLSession, completion: @escaping (Result<Void, Error>) -> Void)
+
+}
+
+extension HTTPModelController {
+
+    public func getModels<Model: Codable & Equatable>(with requestBuilder: HTTP.RequestBuilder, in session: URLSession, completion: @escaping (Result<[Model], Error>) -> Void) {
+        let request: URLRequest = requestBuilder.build()
+        self.sendRequest(request, in: session, completion)
+    }
+
+    public func getModel<Model: Codable & Equatable>(with requestBuilder: HTTP.RequestBuilder, in session: URLSession, completion: @escaping (Result<Model, Error>) -> Void) {
+        let request: URLRequest = requestBuilder.build()
+        self.sendRequest(request, in: session, completion)
+    }
+
+    public func postModel<Model: Codable & Equatable>(_ model: Model, with requestBuilder: HTTP.RequestBuilder, in session: URLSession, completion: @escaping (Result<Model, Error>) -> Void) {
+        let request: URLRequest = requestBuilder.build()
+        self.sendRequest(request, in: session, completion)
+    }
+
+    public func putModel<Model: Codable & Equatable>(_ model: Model, with requestBuilder: HTTP.RequestBuilder, in session: URLSession, completion: @escaping (Result<Model, Error>) -> Void) {
+        let request: URLRequest = requestBuilder.build()
+        self.sendRequest(request, in: session, completion)
+    }
+
+    public func deleteModel<Model: Codable & Equatable>(_ model: Model, with requestBuilder: HTTP.RequestBuilder, in session: URLSession, completion: @escaping (Result<Void, Error>) -> Void) {
+        let request: URLRequest = requestBuilder.build()
+        self.sendRequestExpectingNoContent(request, in: session, completion)
+    }
+
+}
